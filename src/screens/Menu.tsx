f223bf7f71ea05eb5a26299f07fb54617d9d601d
Renan Machado <renan.mav@hotmail.com>
2020-05-05 16:39:07 -0300

    feat(styles): adapt to use tailwind on web

--- src/screens/Menu.tsx
+++ src/screens/Menu.tsx
@@ -1,37 +1,27 @@
 import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
 import { t } from 'react-native-tailwindcss';
-import styled from 'styled-components/native';
 
-const Background = styled.View`
-  ${[t.hFull, t.itemsCenter, t.bgGray500, t.p12, t.pT40]}
-`;
-const Card = styled.View`
-  ${[t.bgWhite, t.wFull, t.p6, t.rounded, t.itemsCenter]}
-`;
-const Title = styled.Text`
-  ${[t.textGray800, t.textXl, t.fontMedium, t.mT0]}
-`;
-const Description = styled.Text`
-  ${[t.textGray600, t.textCenter, t.mT2, t.w56]}
-`;
-const Button = styled.TouchableOpacity`
-  ${[t.bgIndigo600, t.wFull, t.pY2, t.itemsCenter, t.roundedSm, t.mT6]}
-`;
-const ButtonText = styled.Text`
-  ${[t.textWhite, t.fontMedium]}
-`;
+const s = {
+  background: [t.hFull, t.itemsCenter, t.bgGray500, t.p12, t.pT40],
+  card: [t.bgWhite, t.wFull, t.p6, t.rounded, t.itemsCenter],
+  title: [t.textGray800, t.textXl, t.fontMedium, t.mT0],
+  description: [t.textGray600, t.textCenter, t.mT2, t.w56],
+  button: [t.bgIndigo600, t.wFull, t.pY2, t.itemsCenter, t.roundedSm, t.mT6],
+  buttonText: [t.textWhite, t.fontMedium],
+};
 
 const Menu: React.FC = () => {
   return (
-    <Background>
-      <Card>
-        <Title>Payment successful</Title>
-        <Description>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Description>
-        <Button>
-          <ButtonText>Go back to dashboard</ButtonText>
-        </Button>
-      </Card>
-    </Background>
+    <View style={s.background}>
+      <View style={s.card}>
+        <Text style={s.title}>Payment successful</Text>
+        <Text style={s.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</Text>
+        <TouchableOpacity style={s.button}>
+          <Text style={s.buttonText}>Go back to dashboard</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
